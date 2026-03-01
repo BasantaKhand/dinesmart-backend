c9847abf971d2f2ff0f84d05c3e2a912e7d3d27d
Basanta Khand <khandbasanta55@gmail.com>
2026-03-01 19:27:34 +0545

    refactor(config): update environment and package configuration

--- src/routes/payment.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleEsewaWebhook } = require('../controllers/payment.controller');
-
-router.post('/esewa/webhook', handleEsewaWebhook);
-
-module.exports = router;
